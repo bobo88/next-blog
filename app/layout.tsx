4721dd490deff2705d87955f2677d3b64dd3f5ad
Bob <yuanboi88@163.com>
2024-05-23 17:40:20 +0800

    perf: 代码优化

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,9 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import './styles/globals.scss'
-
-const inter = Inter({ subsets: ['latin'] })
-
-// export const metadata: Metadata = {
-//   title: '知行合一',
-//   description: 'ycy88官网',
-//   keywords: 'V8程序, 前端博客小站 - JunF, 技术之外, 思维, 模型, Next'
-// }
-
-// {/* <head>
-//   <title>{metadata.title}</title>
-//   <meta name="description" content={metadata.description} />
-//   <meta name="keywords" content={metadata.keywords} />
-// </head> */}
 
 export default function RootLayout({
   children
 }: Readonly<{
   children: React.ReactNode
 }>) {
-  return (
-    <html lang="en">
-      {children}
-      {/* <body className={inter.className}>{children}</body> */}
-    </html>
-  )
+  return <html lang="en">{children}</html>
 }
